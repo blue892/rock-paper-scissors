f4da5ef93f4a2af3decf2354406e9b2a02457fdf
blue892 <110934835+blue892@users.noreply.github.com>
2024-03-13 12:50:16 -0400

    Created getComputerChoice function in script.js

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 /*
 Rock paper scissors script
 */
+
+// Make global variable for computer choice
+let computerChoice;
+
+/*
+Random number between 0 and 2 inclusive
+0 - "rock"
+1 - "paper"
+2 - "scissors"
+*/
+function getComputerChoice() {
+    let rand = Math.floor(Math.random() * 3);
+    switch (rand) {
+        case 0:
+            computerChoice = "rock";
+            break;
+
+        case 1:
+            computerChoice = "paper";
+            break;
+
+        default:
+            computerChoice = "scissors";
+    }
+}
