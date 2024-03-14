e63c6ab583f62d8528bb6af24e8bc5afdccae30c
blue892 <110934835+blue892@users.noreply.github.com>
2024-03-13 23:08:40 -0400

    Create playGame and getPlayerChoice functions

--- script.js
+++ script.js
@@ -22,15 +22,20 @@ function getComputerChoice() {
     }
 }
 
-let computerChoice = getComputerChoice();
+/*
+Declare playerChoice outside of loop in order to avoid errors
+Keep prompting user for choice until an appropriate value is given
+Return that value
+*/
 
-// Declare the variable globally in order to avoid errors
-let playerChoice;
-
-do {
-    playerChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
+function getPlayerChoice() {
+    let playerChoice;
+    do {
+        playerChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
+    }
+    while(playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
+    return playerChoice;
 }
-while(playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
 
 /*
 If the choices are the same, it's a draw
@@ -57,4 +62,12 @@ function playRound(computerChoice, playerChoice) {
     }
 }
 
-console.log("Player chooses " + playerChoice + " and computer chooses " + computerChoice + ". " + playRound(computerChoice, playerChoice));
+function playGame() {
+    for (let i = 0; i < 5; i++) {
+        let computerChoice = getComputerChoice();
+        let playerChoice = getPlayerChoice();
+        console.log("Player chooses " + playerChoice + " and computer chooses " + computerChoice + ". " + playRound(computerChoice, playerChoice));
+    }
+}
+
+playGame();
