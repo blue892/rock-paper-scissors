4acbe8001497de0c9e8a80435f52f5077b0a497a
blue892 <110934835+blue892@users.noreply.github.com>
2024-03-13 22:53:29 -0400

    Finalized code by implementing the playRound function and printing the result

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ function getComputerChoice() {
     }
 }
 
+getComputerChoice();
+
 // Declare the variable globally in order to avoid errors
 let playerChoice;
 
@@ -34,3 +36,30 @@ do {
     playerChoice = prompt("Choose rock, paper, or scissors").toLowerCase();
 }
 while(playerChoice != "rock" && playerChoice != "paper" && playerChoice != "scissors");
+
+/*
+If the choices are the same, it's a draw
+
+If player chooses rock and computer chooses scissors
+Or if player chooses paper and computer chooses rock
+Or if player chooses scissors and computer chooses paper
+Then player wins
+
+Otherwise, the computer wins
+*/
+
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice === playerChoice) {
+        return "It was a draw.";
+    }
+    else if ((playerChoice === "rock" && computerChoice === "scissors") ||
+            (playerChoice === "paper" && computerChoice === "rock") ||
+            (playerChoice === "scissors" && computerChoice === "paper")) {
+                return "Player wins.";
+            }
+    else {
+        return "Computer wins.";
+    }
+}
+
+console.log("Player chooses " + playerChoice + " and computer chooses " + computerChoice + ". " + playRound(computerChoice, playerChoice));
