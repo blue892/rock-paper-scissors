6b0b818480b48eaf444f4ab3ce0834332f8c481c
blue892 <110934835+blue892@users.noreply.github.com>
2024-03-19 10:51:09 -0400

    Add html elements and updateScreenElements function

--- script.js
+++ script.js
@@ -25,8 +25,13 @@ function getComputerChoice() {
 // Declare score variables globally so they can be accesssed by the playRound function
 let playerScore = 0;
 let computerScore = 0;
+let roundsPlayed = 0;
 
 function playRound(computerChoice, playerChoice) {
+    roundsPlayed++;
+    let selection = document.querySelector("#selectionOutput");
+    selection.textContent = "Computer choice: " + computerChoice +
+                            ". Player choice: " + playerChoice + ".";
     if (computerChoice === playerChoice) {
         return "It was a draw.";
     }
@@ -42,8 +47,30 @@ function playRound(computerChoice, playerChoice) {
     }
 }
 
+function updateScreenElements(currentRound) {
+    if (roundsPlayed <= 5) {
+        let score = document.querySelector("#score");
+        score.textContent = currentRound + 
+                            " Player score: " + playerScore + 
+                            ". Computer score: " + computerScore + ".";
+    }
+    if (roundsPlayed == 5) {
+        let winner = document.querySelector("#winner");
+        if (playerScore > computerScore) {
+            winner.textContent = "Overall winner: Player. ";
+        }
+        else if (playerScore < computerScore) {
+            winner.textContent = "Overall winner: Computer. ";
+        }
+        else {
+            winner.textContent = "Overall winner: None (draw). ";
+        }
+        winner.textContent += "Game ended, no more input accepted.";
+    }
+    // Do not do anything past 5 rounds since the game ends
+}
 
 let selection = document.querySelector("#selection");
 selection.addEventListener("click", (e) => {
-    console.log(e.target.value);
+    updateScreenElements(playRound(getComputerChoice(), e.target.value));
 });
